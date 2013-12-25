895d340a8bdb99d6b30c0f556f634ee671aed917
Matthew Rawcliffe <mar44@aber.ac.uk>
2013-12-25 07:54:48 +0000

    weird mine adding issues

--- minesweeper.c
+++ minesweeper.c
@@ -15,6 +15,15 @@ typedef struct {
     int value, hidden, marked;
 } cell;
 
+int rand_lim(int limit){
+    int div = RAND_MAX / (limit+1);
+    int ret;
+    do {
+        ret = rand() / div;
+    } while(ret > limit);
+    return ret;
+}
+
 cell **initgrid(){
     cell **grid = malloc(sizeof(cell*)*gbufx*gbufy);
     return grid;
@@ -32,8 +41,57 @@ void creategrid(cell **grid){
     }
 }
 
+void addmines(cell **grid){
+    /*
+     * Bounds:
+     * x from GRIDX+1 -> 2*GRIDX
+     * y from GRIDX+1 ->
+    */
+    int count = 0;
+    while(count < MINES){
+        int x = rand_lim(GRIDX);
+        int y = rand_lim(GRIDY);
+        if(grid[GRIDX*y+x]->value != -1
+        && GRIDX*y+x >= GRIDX
+        && GRIDX*y+x <= (GRIDX*GRIDY)+GRIDX){
+            grid[GRIDX*y+x]->value = -1;
+            count++;
+        }
+    }
+}
+
+void printgrid(cell **grid){
+    int i, j;
+    for(j = 0; j < gbufy; j++){
+        for(i = 0; i < gbufx; i++){
+            if(grid[GRIDX*j+i]->value == -1){
+                start_color();
+                init_pair(1, COLOR_RED, COLOR_BLACK);
+                attron(COLOR_PAIR(1));
+                printw("(%d) %d ", GRIDX*j+i, grid[GRIDX*j+i]->value);
+                attroff(COLOR_PAIR(1));
+            } else {
+                printw("(%d) %d ", GRIDX*j+i, grid[GRIDX*j+i]->value);
+            }
+        }
+        printw("\n");
+    }
+}
+
 int main(void){
+    srand(time(NULL));
     cell **grid = initgrid();
     creategrid(grid);
+    //addmines(grid);
+    // ncurses
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    curs_set(0);
+    printgrid(grid);
+    refresh();
+    getch();
+    endwin();
     return 0;
 }
